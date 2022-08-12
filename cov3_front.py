d0ac22deec9bad320cf95fd03160269345f62a58
Tiago Galdino <tiagogaldino@gmail.com>
2022-08-12 17:20:23 -0300

    Aula 47 (implementado rotas de usuários, ADMIN e GESTOR.)

--- cov3_front.py
+++ cov3_front.py
@@ -3,6 +3,23 @@ from cov3_back2 import diagnostico
 from cov3_back3 import filtrox, consolida_cid, consolida_dig
 import random
 
+def login(usuarios):
+    print("\nDigite seu Código de Usuário: ")
+    cod = input()
+    for chave in usuarios:
+        subdic = usuarios.get(chave)
+        for subchave in subdic:
+            if cod == subdic.get(subchave):
+                print("Digite sua Senha:")
+                password = input()
+                if password == subdic.get("Senha"):
+                    perf = subdic.get("Perfil")
+                    return perf
+                else:
+                    print("Senha Inválida")
+                    return 0
+
+
 
 def gerasaudacao(saudacoes):
     tam = len(saudacoes)
@@ -13,54 +30,83 @@ def gerasaudacao(saudacoes):
 
 
 def menu(usuarios, cidadaos, saudacoes, sintomas, diagnosticos):
-    x = 99
-    while x == 99:
-        tam = len(saudacoes)
-        if tam < 1:
-            print("\n\n - - - Olá, Seja Bem Vind[x] - - - ")
-        else:
-            gerasaudacao(saudacoes)
+    tam = len(saudacoes)
+    if tam < 1:
+        print("\n\n --- Olá, Seja Bem Vind[x] ----")
+    else:
+        gerasaudacao(saudacoes)
 
-        print("\n--- MENU ---")
-        rota = int(input("\n[1] Cadastrar | [2] Listar | [3] Diagnóstico | [4] Relatórios | [5] Sair "))
-        if rota == 1:
-            print("\n\n--- SUB MENU ---")
-            subrota = int(input("\n[1] Cadastrar Usuário | [2] Cadastrar Cidadãos | [3] Cadastrar Saudações | [4] Cadastrar Sintomas "))
-            if subrota == 1:
-                cad_user(usuarios)
-            elif subrota == 2:
-                subcadcid = int(input("\n ---- [1] Importar Cidadão | [2] Cadastro Manual ---- "))
-                if subcadcid == 1:
-                    imp_cid(cidadaos)
-                if subcadcid == 2:
-                    cad_cidadaos(cidadaos)
-            elif subrota == 3:
-                cad_saudacoes(saudacoes)
-            elif subrota == 4:
-                cad_sintomas(sintomas)
-        if rota == 2:
-            print("\n\n--- SUB MENU ---")
-            subrota = int(input("\n[1] Listar Usuários | [2] Listar Cidadãos | [3] Listar Saudações | [4] Listar Sintomas "))
-            if subrota == 1:
-                list_user(usuarios)
-            elif subrota == 2:
-                list_cidadaos(cidadaos)
-            elif subrota == 3:
-                list_saudacoes(saudacoes)
-            elif subrota == 4:
-                list_sintomas(sintomas)
-        if rota == 3:
-            diagnostico(cidadaos, sintomas, diagnosticos)
-        if rota == 4:
-            print("\n\n --- SUB MENU ---")
-            subrota = int(input("\n [1] Consolidados de Cidadãos | [2] Consolidado de Diagnósticos | [3] Filtros Cruzados"))
-            if subrota == 1:
-                consolida_cid(cidadaos)
-            elif subrota == 2:
-                consolida_dig(cidadaos, diagnosticos)
-            elif subrota == 3:
-                filtrox(cidadaos, sintomas, diagnosticos)
-        if rota == 5:
-            x = 5
-            print("\n\nPROGRAMA FINALIZADO \n\n")
-            exit()
+# Implementando nova entrada de usuário
+    print(" --- Digite [1] Login ou [2] Sair")
+    entrar = int(input())
+    while entrar < 1 or entrar > 2:
+        print("\nEntrada inválida!")
+        print(" --- Digite [1] Login ou [2] Sair")
+        entrar = int(input())
+    if entrar == 2:
+        print("\nPrograma Finalizado")
+        exit()
+    else:
+        y = 88
+        while y == 88:
+            acesso = login(usuarios)
+            if acesso == "Admin":
+                x = 99                          # código já implementando.
+                while x == 99:
+                    print("\n--- MENU ---")
+                    rota = int(input("\n[1] Cadastrar | [2] Listar | [3] Diagnóstico | [4] Relatórios | [5] Sair "))
+                    if rota == 1:
+                        print("\n\n--- SUB MENU ---")
+                        subrota = int(input("\n[1] Cadastrar Usuário | [2] Cadastrar Cidadãos | [3] Cadastrar Saudações | [4] Cadastrar Sintomas "))
+                        if subrota == 1:
+                            cad_user(usuarios)
+                        elif subrota == 2:
+                            subcadcid = int(input("\n ---- [1] Importar Cidadão | [2] Cadastro Manual ---- "))
+                            if subcadcid == 1:
+                                imp_cid(cidadaos)
+                            if subcadcid == 2:
+                                cad_cidadaos(cidadaos)
+                        elif subrota == 3:
+                            cad_saudacoes(saudacoes)
+                        elif subrota == 4:
+                            cad_sintomas(sintomas)
+                    if rota == 2:
+                        print("\n\n--- SUB MENU ---")
+                        subrota = int(input("\n[1] Listar Usuários | [2] Listar Cidadãos | [3] Listar Saudações | [4] Listar Sintomas "))
+                        if subrota == 1:
+                            list_user(usuarios)
+                        elif subrota == 2:
+                            list_cidadaos(cidadaos)
+                        elif subrota == 3:
+                            list_saudacoes(saudacoes)
+                        elif subrota == 4:
+                            list_sintomas(sintomas)
+                    if rota == 3:
+                        diagnostico(cidadaos, sintomas, diagnosticos)
+                    if rota == 4:
+                        print("\n\n --- SUB MENU ---")
+                        subrota = int(input("\n [1] Consolidados de Cidadãos | [2] Consolidado de Diagnósticos | [3] Filtros Cruzados"))
+                        if subrota == 1:
+                            consolida_cid(cidadaos)
+                        elif subrota == 2:
+                            consolida_dig(cidadaos, diagnosticos)
+                        elif subrota == 3:
+                            filtrox(cidadaos, sintomas, diagnosticos)
+                    if rota == 5:
+                        x = 5
+                        print("\n\nPROGRAMA FINALIZADO \n\n")
+                        exit()
+
+            if acesso == "Gestor":
+                print("\n\n --- MENU GESTOR ---")
+                subrota = int(input("\n [1] Consolidados de Cidadãos | [2] Consolidado de Diagnósticos | [3] Filtros Cruzados | [4] Sair"))
+                if subrota == 1:
+                    consolida_cid(cidadaos)
+                elif subrota == 2:
+                    consolida_dig(cidadaos, diagnosticos)
+                elif subrota == 3:
+                    filtrox(cidadaos, sintomas, diagnosticos)
+                elif subrota == 4:
+                    x = 4
+                    print("\n\nPROGRAMA FINALIZADO \n\n")
+                    exit()
--- cov3_pri.py
+++ cov3_pri.py
@@ -1,6 +1,17 @@
 from cov3_front import menu
 
-usuarios = {}
+usuarios = {
+    1: {
+    "Nome": "Silva",
+    "Código": "131313",
+    "Senha": "L13",
+    "Perfil": "Admin"},
+    2: {
+    "Nome": "Ana",
+    "Código": "141414",
+    "Senha": "L14",
+    "Perfil": "Gestor"},
+    }
 cidadaos = {}
 saudacoes = {}
 sintomas = {}
